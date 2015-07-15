e3c72f8979f761fc958943c63778e401dfda4ade
PavSingh <pav2013@yahoo.com>
2015-07-15 15:59:35 -0400

    Project Started

---
+++ Project-Website-SM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_Website_SM.Global" Language="C#" %>
---
+++ Project-Website-SM/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Project_Website_SM
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
